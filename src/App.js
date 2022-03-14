aa93097d61032686cca4b9b22f31b09ea815589c
evgenymozh <evgenymozhaev90@yandex.ru>
2022-03-14 14:31:59 +0500

    Proptypes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components";
 import { Cart, Home } from "./pages";
+import axios from 'axios'
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({data}) => {
+      setPizzas(data.pizzas);
+    })
+    
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<Home items={pizzas}/>} />
+          <Route path="/cart" element={<Cart />} exact/>
         </Routes>
-        
       </div>
     </div>
   );
