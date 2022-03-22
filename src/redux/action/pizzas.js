0ae9743d6cf3e9aa480ebf2600eddd84c266316f
evgenymozh <evgenymozhaev90@yandex.ru>
2022-03-22 12:02:55 +0500

    final

--- src/redux/action/pizzas.js
+++ src/redux/action/pizzas.js
@@ -11,7 +11,7 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
         type: 'SET_LOADED',
         payload: false,
       });
-  axios.get(`http://localhost:3005/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
+  axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
     dispatch(setPizzas(data));
   });
 };
